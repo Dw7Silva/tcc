4f6b34f748f8418da71cfabe5001d176f72c8279
niel02k <nielfreitasfree@gmail.com>
2025-03-13 11:45:29 -0300

    13-03  11:45

--- src/app/inicio/page.jsx
+++ src/app/inicio/page.jsx
@@ -1,9 +1,25 @@
-import styles from "./home.nodule.css";
+import styles from "./home.module.css";
+import {GoHomeFill } from "react-icons/go"
+import { FaSearch } from "react-icons/fa";
+import { IoChatbox } from "react-icons/io5";
+import { MdSupportAgent } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+
+
+
+
+
 
 export default function Inicio() {
     return(
-        <div className='styles.container'>
+        <div className={styles.container}>
             <h1>Inicio</h1>
+            <GoHomeFill size={30} color="A87453" />
+      <FaSearch size={30} color="A87453" />
+      <IoChatbox size={30} color="A87453" />
+      <MdSupportAgent size={30} color="A87453" />
+      <FaUser size={30} color="A87453" />
+
         </div>
-    )
+    );
 } 
