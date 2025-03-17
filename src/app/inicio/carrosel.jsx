419a3e3a66e5838f693aa5f9e7a04a23921515f9
niel02k <nielfreitasfree@gmail.com>
2025-03-17 09:01:22 -0300

    17/03

--- src/app/inicio/carrosel.jsx
+++ src/app/inicio/carrosel.jsx
@@ -1,6 +1,28 @@
-  import { register} from 'swiper/element/bundle';
-  import 'swiper/css';
-  import 'swiper/css/navigation';
-  import 'swiper/css/pagination';
-  import 'swiper/css/scrollbar';
-  register ();
+"use client";
+
+import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+const Carousel = ({ images }) => {
+  return (
+    <div style={{ width: "100%", textAlign: "center" }}>
+      <Splide
+        options={{ perPage: 1, gap: "1rem", autoplay: true, interval: 5000 }}
+        style={{ margin: "0 auto" }}
+      >
+        {images.map((img, index) => (
+          <SplideSlide key={index}>
+            <img
+              src={img}
+              alt={`Slide ${index + 1}`}
+              style={{ borderRadius: "10px" }} // Adicione esta linha
+            />
+          </SplideSlide>
+        ))}
+      </Splide>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/app/inicio/page.jsx
+++ src/app/inicio/page.jsx
@@ -5,34 +5,45 @@ import { IoChatbox } from "react-icons/io5";
 import { MdSupportAgent } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { HiOutlineMenu } from "react-icons/hi";
-
-
-
+import Carousel from "./carrosel";
 
 
 export default function Inicio() {
-    return (
-        <>
-          <div className={styles.navbar}>
-            <div className={styles.logoNav}>Meu Site</div>
-            <div className={styles.searchBar}>
-              <input type="text" placeholder="Pesquisar..." />
-              <button>
-                <FaSearch />
-              </button>
-            </div>
-            <div className={styles.navIcons}>
-              <GoHomeFill />
-              <IoChatbox />
-              <MdSupportAgent />
-              <FaUser />
-              <HiOutlineMenu />
-            </div>
-          </div>
-    
-         
-           
-             
-        </>
-      );
-    }
+
+    const Carroselimages = [
+        "https://placehold.co/600x400/EEE/31343C",
+        "https://placehold.co/600x400/CCC/41444C",
+        "https://placehold.co/600x400/AAA/51545C",
+        "https://placehold.co/600x400/888/61646C",
+        "https://placehold.co/600x400/666/71747C",
+      ];
+  return (
+
+
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <div className={styles.logoNav}>Meu Site</div>
+
+        <div className={styles.searchBar}>
+          <input type="text" placeholder="Pesquisar..." />
+          <button>
+            <FaSearch />
+          </button>
+        </div>
+
+        <div className={styles.navIcons}>
+          <GoHomeFill />
+          <IoChatbox />
+          <MdSupportAgent />
+          <FaUser />
+          <HiOutlineMenu />
+        </div>
+      </div>
+
+      <div className={styles.carouselContainer}> {/* Adicione este container */}
+        <Carousel images={Carroselimages} />
+      </div>
+      
+    </div>
+  );
+}
