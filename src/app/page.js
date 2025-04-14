03f8922b8271558034817166d9e03f98f26b5a49
niel02k <nielfreitasfree@gmail.com>
2025-04-14 14:33:48 -0300

    14/04 14:33

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,14 @@ import React from "react";
 import Proposta from "./proposta/page";
 import Inicio from "./inicio/page";
 import Login from "./login/page";
-
-
+import CriarOferta from "./oferta/page";
 export default function dev() {
 
 
 
 
   return (
-    <Inicio></Inicio>
+    <CriarOferta></CriarOferta>
   );
 }
 
