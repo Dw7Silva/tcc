b73e92ad3efe92ac45b2831af205f80602277a4e
fabricio861 <fabriciomansano340@gmail.com>
2025-07-29 11:03:58 -0300

    29/07

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -6,6 +6,7 @@ const Logo = "https://i.ibb.co/23YGGMNM/Logo-Transparente.png";
 export default function Home() {
   return (
     <div>
+      
       <div className={styles.navbr}>
         <div>
           <img src={Logo} className={styles.logo} alt="Logo" />
