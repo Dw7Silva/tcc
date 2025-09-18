7831c74c07dd91d7c114fc77c6ea52d3829ee412
fabricio861 <fabriciomansano340@gmail.com>
2025-09-18 10:39:34 -0300

    22/03

--- src/mockup/ofertas.js
+++ src/mockup/ofertas.js
@@ -9,6 +9,9 @@ const ofertas = [
     oferta_data_colheita: "2025-01-15",
     oferta_outras_informacoes: "Produção sem pesticidas",
     oferta_data_publicacao: "2025-03-10",
+    agricultor_nome:"Ricardo",
+    amendoim_tipo: "Runner (com casca)",
+    imagem: "https://img.olx.com.br/images/35/354519630978865.webp",
     oferta_ativa: 1
   },
   {
@@ -20,6 +23,9 @@ const ofertas = [
     oferta_data_colheita: "2024-12-20",
     oferta_outras_informacoes: "Amendoim de alta qualidade",
     oferta_data_publicacao: "2025-03-12",
+    agricultor_nome:"Ricardo",
+    amendoim_tipo: "Runner (com casca)",
+    imagem: "https://img.olx.com.br/images/35/354519630978865.webp",
     oferta_ativa: 1
   }
 ]
